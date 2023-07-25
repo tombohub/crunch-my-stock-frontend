4fe079273fd0c7d3ed4faf0037e682d58f02cbe0
me <tkutlesa@gmail.com>
2023-07-25 11:01:47 -0400

    Revert "removed react query"
    This reverts commit 8212b74c69e25b153b55281ecfcf4bba06606948.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { ChakraProvider, Input, Container } from "@chakra-ui/react";
 import Button from "./components/visual/Button";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-import axios from "axios";
-import Layout from "./components/Layout";
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <ChakraProvider>
-        <Layout />
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Container>
+            <Input type="date" />
+          </Container>
+        </ChakraProvider>
+      </QueryClientProvider>
     </>
   );
 }
