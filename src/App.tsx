89045a2f900e91a572296b306d558e615d59a414
me <tkutlesa@gmail.com>
2023-07-25 10:10:46 -0400

    added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { ChakraProvider, Input, Container } from "@chakra-ui/react";
 import Button from "./components/visual/Button";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <ChakraProvider>
-        <Container>
-          <Input type="date" />
-        </Container>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Container>
+            <Input type="date" />
+          </Container>
+        </ChakraProvider>
+      </QueryClientProvider>
     </>
   );
 }
